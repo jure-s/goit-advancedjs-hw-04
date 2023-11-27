c16fcc60b283d02853f4ce239a728746abcff24f
jure-s <71380578+jure-s@users.noreply.github.com>
2023-11-28 00:12:07 +0200

    fix bugs

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,8 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 iziToast.settings({
   position: 'topRight',
+  timeout: 3000,
+  close: false,
 });
 
 const apiKey = '40910932-9fe08a71e8ac0f3a1299db850';
@@ -14,7 +16,9 @@ const gallery = document.querySelector('.gallery');
 let page = 1;
 let currentSearchQuery = '';
 const lightbox = new SimpleLightbox('.gallery a');
-let hoorayDisplayed = false; // Флаг для відстеження виведення повідомлення "Hooray"
+let hoorayDisplayed = false;
+let allImagesLoaded = false;
+let noMoreImagesToastDisplayed = false;
 
 searchForm.addEventListener('submit', async (event) => {
   event.preventDefault();
@@ -23,114 +27,91 @@ searchForm.addEventListener('submit', async (event) => {
   const formData = new FormData(searchForm);
   const searchQuery = formData.get('searchQuery');
   currentSearchQuery = searchQuery.trim();
-  await searchImages(currentSearchQuery);
+  
+  hoorayDisplayed = false;
+  allImagesLoaded = false;
+  noMoreImagesToastDisplayed = false;
+
+  if (currentSearchQuery !== '') {
+    const data = await searchImages(currentSearchQuery);
+    handleImageData(data);
+  } else {
+    iziToast.info({
+      title: 'Info',
+      message: 'Please enter a search query.',
+    });
+  }
 });
 
 async function searchImages(query) {
-  try {
-    const response = await axios.get('https://pixabay.com/api/', {
-      params: {
-        key: apiKey,
-        q: query,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        page: page,
-        per_page: 40,
-      },
-    });
+  const response = await axios.get('https://pixabay.com/api/', {
+    params: {
+      key: apiKey,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: page,
+      per_page: 40,
+    },
+  });
 
-    if (response.status === 200) {
-      const responseData = response.data;
-      if (
-        responseData &&
-        responseData.hits &&
-        Array.isArray(responseData.hits) &&
-        responseData.totalHits !== undefined
-      ) {
-        const images = responseData.hits;
-        const totalHits = responseData.totalHits;
+  return response.data;
+}
 
-        if (images.length === 0) {
-          iziToast.error({
-            title: 'Error',
-            message: 'Sorry, there are no images matching your search query. Please try again.',
-          });
-        } else {
-          displayImages(images);
-          if (!hoorayDisplayed) {
-            iziToast.success({
-              title: 'Success',
-              message: `Hooray! We found ${totalHits} images.`,
-            });
-            hoorayDisplayed = true;
-          }
-          lightbox.refresh();
-          page++;
-        }
-      } else {
-        iziToast.error({
-          title: 'Error',
-          message: 'Response format is incorrect. Please try again.',
-        });
-      }
-    } else {
-      iziToast.error({
-        title: 'Error',
-        message: 'Failed to fetch images. Please try again.',
-      });
-    }
-  } catch (error) {
-    console.error('Error fetching images:', error);
+function handleImageData(data) {
+  if (data.hits.length === 0) {
     iziToast.error({
       title: 'Error',
-      message: 'Failed to fetch images. Please try again.',
+      message: 'Sorry, there are no images matching your search query. Please try again.',
     });
+  } else {
+    displayImages(data.hits);
+    if (!hoorayDisplayed) {
+      iziToast.success({
+        title: 'Success',
+        message: `Hooray! We found ${data.totalHits} images.`,
+      });
+      hoorayDisplayed = true;
+    }
+    lightbox.refresh();
+    page++;
+    if (data.hits.length < 40) {
+      allImagesLoaded = true;
+    }
   }
 }
 
 function displayImages(images) {
+  let imagesHTML = '';
+
   images.forEach((image) => {
-    const cardLink = document.createElement('a');
-    cardLink.href = image.largeImageURL;
-    cardLink.setAttribute('data-lightbox', 'gallery-images');
+    const infoHTML = `
+      <div class="info">
+        <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+        <p class="info-item"><b>Views:</b> ${image.views}</p>
+        <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+        <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+      </div>
+    `;
 
-    const card = document.createElement('div');
-    card.classList.add('photo-card');
+    const cardLink = `
+      <a href="${image.largeImageURL}" data-lightbox="gallery-images">
+        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy">
+      </a>
+    `;
 
-    const img = document.createElement('img');
-    img.src = image.webformatURL;
-    img.alt = image.tags;
-    img.loading = 'lazy';
-    cardLink.appendChild(img);
-    card.appendChild(cardLink);
+    const card = `
+      <div class="photo-card">
+        ${cardLink}
+        ${infoHTML}
+      </div>
+    `;
 
-    const info = document.createElement('div');
-    info.classList.add('info');
-
-    const likes = document.createElement('p');
-    likes.classList.add('info-item');
-    likes.innerHTML = `<b>Likes:</b> ${image.likes}`;
-    info.appendChild(likes);
-
-    const views = document.createElement('p');
-    views.classList.add('info-item');
-    views.innerHTML = `<b>Views:</b> ${image.views}`;
-    info.appendChild(views);
-
-    const comments = document.createElement('p');
-    comments.classList.add('info-item');
-    comments.innerHTML = `<b>Comments:</b> ${image.comments}`;
-    info.appendChild(comments);
-
-    const downloads = document.createElement('p');
-    downloads.classList.add('info-item');
-    downloads.innerHTML = `<b>Downloads:</b> ${image.downloads}`;
-    info.appendChild(downloads);
-
-    card.appendChild(info);
-    gallery.appendChild(card);
+    imagesHTML += card;
   });
+
+  gallery.insertAdjacentHTML('beforeend', imagesHTML);
 }
 
 function checkScroll() {
@@ -141,7 +122,17 @@ function checkScroll() {
   } = document.documentElement;
 
   if (scrollTop + clientHeight >= scrollHeight - 5) {
-    searchImages(currentSearchQuery);
+    if (!allImagesLoaded && !noMoreImagesToastDisplayed) {
+      searchImages(currentSearchQuery).then((data) => {
+        handleImageData(data);
+      });
+    } else if (allImagesLoaded && !noMoreImagesToastDisplayed) {
+      iziToast.info({
+        title: 'Info',
+        message: 'No more images to load.',
+      });
+      noMoreImagesToastDisplayed = true;
+    }
   }
 }
 
